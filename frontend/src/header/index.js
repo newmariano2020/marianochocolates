cb2a0c76868cb83c7c9bd4fc87e9f604d41d6ae6
Emanuel Friedrich <emanuel.friedrich@onetree.com>
2021-12-10 23:33:58 -0300

    Icons

--- frontend/src/header/index.js
+++ frontend/src/header/index.js
@@ -1,23 +1,28 @@
-import { faIndustry } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
-const Header = () => <div className="header">
-  <div class="logo">
-    <img src="/img/logo.png" alt="La Pinocha logo"></img>
-  
-  </div>
-  <div class="nav-menu">
-    <ul>
+const Header = () => (
+  <div className="header">
+    <div class="logo">
+      <img src="/img/logo.png" alt="La Pinocha logo"></img>
+    </div>
+    <div class="nav-menu">
+      <ul>
         <li>
           <Link to="/productos">Comprá Online</Link>
           <Link to="/conocenos">Conócenos</Link>
           <Link to="/sucursales">Sucursales</Link>
           <Link to="/franquicia">Franquicia</Link>
-          <FontAwesomeIcon icon={faIndustry}></FontAwesomeIcon>
+          
+          <div className="social">
+            <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+          </div>
         </li>
       </ul>
-      </div>
-</div>;
+    </div>
+  </div>
+);
 
 export default Header;
