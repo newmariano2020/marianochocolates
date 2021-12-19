8be25acbb8f443aa07bc81f9bdb2abfcf7a14d1b
cirilliluis@gmailcom3VU0UO4\Luis <Luis Mariano Cirilli>
2021-12-19 20:27:01 -0300

    arreglos en la home y un desarreglo

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,61 +1,64 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="upper-section">
-        <div className="left-footer">
-          <div className="logo-footer">
+    <div className="container footer">
+      <div className="row upper-sections">
+        <div classname = "col-2">
+        <div className="logo-footer">
             <img src={require("../img/logo-footer.png").default} alt=""></img>
           </div>
-          <div className="text-left-footer">
-            <span className="title">La Pinocha Chocolates</span>
-            <span className="bajada-de-linea">
-              Nacimos en el mar. Crecimos en el bosque
-            </span>
-          </div>
+
         </div>
-        <div className="right-footer">
-          <div className="right-footer-text">
-            <div className="contact">
-              <span className="contact-us-font">Escribinos!</span>
-            </div>
-            <div className="footer-message">
-              <span className="footer-message-font">Estamos para ayudarte</span>
-            </div>
+        <div className="right-footer-content">
+          <div className="right-footer-content_group-1">
+
+            <span className="right-footer-content_title">La Pinocha Chocolates</span>
+          </div>
+          <div>
+              <span className="right-footer-content_bajada-de-linea">
+                Nacimos en el mar. Crecimos en el bosque
+              </span>
+          </div>
+          <div className="right-footer-content_group-2">
+            <span className="contact-us-font">Escribinos!</span>
+            <span className="footer-message-font">Estamos para ayudarte</span>
+          </div>
+          <div>
             <a href="mailto:info@lapinochachocolates.com">
               <span className="contact-em">info@lapinochachocolates.com</span>
             </a>
           </div>
-        </div>
-        <div className="follow-us">
-          <div className="follow-text">
-            <span>Seguinos</span>
+          <div className="follow-us">
+            <div className="follow-text">
+              <span>Seguinos</span>
+            </div>
+            <div className="footer-logo-right">
+              <a
+                href="https://www.instagram.com/lapinochachocolates"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={require("../img/instagram-relleno.png").default}
+                  alt=""
+                  className="instagram-relleno"
+                ></img>
+              </a>
+              <a
+                href="https://es-la.facebook.com/lapinochachocolates"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={require("../img/facebook-relleno.png").default}
+                  alt=""
+                ></img>
+              </a>
+            </div>
           </div>
-          <div className="footer-logo-right">
-            <a
-              href="https://www.instagram.com/lapinochachocolates"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={require("../img/instagram-relleno.png").default}
-                alt=""
-                className="instagram-relleno"
-              ></img>
-            </a>
-            <a
-              href="https://es-la.facebook.com/lapinochachocolates"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={require("../img/facebook-relleno.png").default}
-                alt=""
-              ></img>
-            </a>
-          </div>
+
         </div>
       </div>
-      <div className="rights">
+      <div className="row rights">
         Todos los derechos reservados 2021 | Diseño por woopi.com.ar
       </div>
     </div>
--- src/pages/home/destacados-section/index.js
+++ src/pages/home/destacados-section/index.js
@@ -1,24 +1,26 @@
 import MiddleSection from "./middle-section";
 
 const Destacados = () => {
-  return <div className="destacados">
-    <div className="destacado destacado-1">
+  return <div className="container container-destacados destacados">
+    <div className="row row-destacados"> 
+    <div className="col-sm-3 col-md-3 col-xs-1 destacado destacado-1">
      <MiddleSection
       message="Estamos Cerca"
       callToActionText="SUCURSALES"
      ></MiddleSection>
     </div>
-    <div className="destacado destacado-2">
+    <div className="col-sm-3 col-md-3 col-xs-1 destacado destacado-2">
      <MiddleSection
       message="Sabores Supremos"
       callToActionText="PRODUCTOS"
      ></MiddleSection>
     </div>
-    <div className="destacado destacado-3">
+    <div className="col-sm-3 col-md-3 col-xs-1 destacado destacado-3">
      <MiddleSection
       message="Somos La Pinocha"
       callToActionText="CONOCENOS"
      ></MiddleSection>
+    </div>
     </div>
   </div>
 }
